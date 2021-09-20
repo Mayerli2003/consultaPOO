cc73da22148b2a112901fc33b0502bfe84d4c4e5
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-09-20 17:40:12 -0600

    Ejercicio 20 de septiembre

---
+++ Clase lunes 20 de septiembre/ConsoleApp1/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Persona
+    {
+        private string nombre;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
